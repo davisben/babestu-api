74f0dc36762de12ea7ab30351406b75463f87927
Ben Davis <ben@davisben.com>
2020-09-08 22:24:56 -0400

    Setup for testing

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,35 @@
 
 use rocket_contrib::databases::rusqlite;
 
-#[database("babestu")]
-struct Connection(rusqlite::Connection);
-
 #[get("/")]
 fn index() -> &'static str {
     "Welcome to babestu!"
 }
 
 pub fn rocket() -> rocket::Rocket {
+    #[database("babestu")]
+    struct DbConn(rusqlite::Connection);
+
     run_migrations();
 
     rocket::ignite()
-        .attach(Connection::fairing())
-        .mount("/", routes![index])
+        .attach(DbConn::fairing())
+        .mount("/", routes())
+}
+
+pub fn rocket_testing() -> rocket::Rocket {
+    #[database("testing")]
+    struct DbConn(rusqlite::Connection);
+
+    run_migrations();
+
+    rocket::ignite()
+        .attach(DbConn::fairing())
+        .mount("/", routes())
+}
+
+fn routes() -> Vec<rocket::Route> {
+    routes![index]
 }
 
 fn run_migrations() {
