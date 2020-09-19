0b82301ac9d18d2fd49002e0318540868fa8aa69
Ben Davis <ben@davisben.com>
2020-09-18 20:46:53 -0400

    Add ErrorResponse

--- src/error.rs
+++ src/error.rs
@@ -1,34 +1,72 @@
 use rocket::Catcher;
-use rocket_contrib::json::JsonValue;
+use rocket::Request;
+use rocket::response::{Responder, Response, Result};
+use rocket::http::{ContentType, Status};
+use serde::Serialize;
+
+pub struct ErrorResponse {
+    pub status: Status,
+    pub message: String,
+}
+
+#[derive(Serialize)]
+struct ErrorResponseData {
+    status: String,
+    message: String,
+}
+
+impl<'r> Responder<'r, 'static> for ErrorResponse {
+    fn respond_to(self, request: &'r Request<'_>) -> Result<'static> {
+        let response_data = ErrorResponseData {
+            status: String::from("error"),
+            message: self.message
+        };
+        let json = json!(response_data);
+
+        Response::build_from(json.respond_to(&request).unwrap())
+            .status(self.status)
+            .header(ContentType::JSON)
+            .ok()
+    }
+}
 
 pub fn catchers() -> Vec<Catcher> {
     catchers![
         not_found,
+        bad_request,
         unprocessable_entity,
         server_error
      ]
 }
 
 #[catch(404)]
-fn not_found() -> JsonValue {
-    json!({
-        "status": 404,
-        "message": "Not found"
-    })
+fn not_found() -> ErrorResponse {
+    ErrorResponse {
+        status: Status::NotFound,
+        message: String::from("Not found")
+    }
+}
+
+#[catch(400)]
+fn bad_request() -> ErrorResponse {
+    ErrorResponse {
+        status: Status::BadRequest,
+        message: String::from("Bad request")
+    }
 }
 
 #[catch(422)]
-fn unprocessable_entity() -> JsonValue {
-    json!({
-        "status": 422,
-        "message": "Unprocessable Entity"
-    })
+fn unprocessable_entity() -> ErrorResponse {
+    ErrorResponse {
+        status: Status::UnprocessableEntity,
+        message: String::from("Unprocessable entity")
+    }
 }
 
 #[catch(500)]
-fn server_error() -> JsonValue {
-    json!({
-        "status": 500,
-        "message": "Internal Server Error"
-    })
+fn server_error() -> ErrorResponse {
+    ErrorResponse {
+        status: Status::InternalServerError,
+        message: String::from("Internal server error")
+    }
 }
